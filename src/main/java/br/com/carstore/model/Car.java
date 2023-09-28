a9582ac0a274c50031b9ec6ead66ed8cf6a0f508
Salomaofl <salomofelipe@hotmail.com>
2023-09-27 21:32:54 -0300

    Adicionando a classe DAO e implementando o Banco de dados

---
+++ src/main/java/br/com/carstore/model/Car.java
@@ -0,0 +1,18 @@
+package br.com.carstore.model;
+
+public class Car {
+    public Car(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,5 +1,8 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +19,9 @@ public class CreateCarServlet extends HttpServlet {
         String carName = request.getParameter("car-name");
 
         System.out.println(carName);
+
+        Car car  = new Car(carName);
+        new CarDao().createCar(car);
 
         request.getRequestDispatcher("index.html").forward(request, response);
 
